dcdde0696d7ed7d9776ca028a22e35fa9be4a909
Tonya <tsims25@gmail.com>
2012-02-26 12:44:23 -0600

    re commit

--- db/migrate/20120224014522_add_foreign_key_column_identities.rb
+++
@@ -1,9 +0,0 @@
-class AddForeignKeyColumnIdentities < ActiveRecord::Migration
-  def self.up
-		add_column :identities, :player_id, :integer
-  end
-
-  def self.down
-		remove_column :identities, :player_id
-  end
-end
