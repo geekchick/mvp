9d5017539d7a8273d01650cd27da552c8d2b676b
Tonya <tsims25@gmail.com>
2012-03-19 08:22:04 -0500

    remove index

--- db/migrate/20120311215933_adding_index_microposts.rb
+++
@@ -1,9 +0,0 @@
-class AddingIndexMicroposts < ActiveRecord::Migration
-  def self.up
-		add_index :microposts, [:user_id, :created_at]
-  end
-
-  def self.down
-		remove_index :microposts
-  end
-end
