b9e620107574a68d3ddfa457d8edd0135d9cc9be
Tonya <tsims25@gmail.com>
2012-03-19 14:48:13 -0500

    revise

--- db/migrate/20120312235246_add_index_microposts.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexMicroposts < ActiveRecord::Migration
-  def self.up
-		add_index :microposts, [:user_id, :created_at]
-  end
-
-  def self.down
-		add_index :microposts, [:user_id, :created_at]
-  end
-end
