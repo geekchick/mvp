07854d0608e4a597f5158eb1048b6274eae032ff
Tonya <tsims25@gmail.com>
2012-03-22 16:11:36 -0500

    twitter changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
 	#protect_from_forgery
 
+
+
 def create
   auth = request.env["omniauth.auth"]
   user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
