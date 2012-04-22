636125e7bbea6612fcac6cf9127972a94efc6e4d
Tonya <tsims25@gmail.com>
2012-04-21 21:14:17 -0500

    add changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 	def avatar_url(user)
-		default_url = "http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm"
+		default_url = "http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&?s=60"
 		gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-		"http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=60&d=#{CGI.escape(default_url)}"
 
 	end
 end
